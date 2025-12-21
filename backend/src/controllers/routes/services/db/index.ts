a18e266278577384c5295779dce899a340196131
mafruhamanal <mafruhamanal1@gmail.com>
2025-12-21 18:48:15 -0500

    express app setup

--- backend/src/controllers/routes/services/db/index.ts
+++ backend/src/controllers/routes/services/db/index.ts
@@ -1,1 +1,18 @@
 // express server will go here
+import express, { Request, Response } from 'express';
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request , res: Response) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/get', (req: Request , res: Response) => {
+  res.send('Wow World!')
+})
+
+app.listen(port, () => {
+  console.log(`vid journal app listening on port ${port}`)
+})
