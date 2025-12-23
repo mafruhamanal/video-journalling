8e3fae7371090b5b2fdc2591767d5b4160c477ad
mafruhamanal <mafruhamanal1@gmail.com>
2025-12-23 16:27:46 -0500

    db connection works yay

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,17 +1,32 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
+import * as schema from './schema.js';
 
-const sqlite = new Database('sqlite.db');
-export const db = drizzle(sqlite);
+const sqlite = new Database('./sqlite.db'); 
+export const db = drizzle(sqlite, { schema });
 // exports a Drizzle db instance to use throughout app to run queries
 // schema can be defined separately in a schema file 
 
 // 
+// const result = await db.query.users.findMany({
+// 	with: {
+// 		videos: true			
+// 	},
+// });
 // const result = await db.insert(users).values({
 //     name: 'John Doe',
 //     email: 'john.doe@example.com',
-//     age: 30
 // });
+
+// const result = await db.insert(schema.users).values( {
+//     firstName: 'Mafruha',
+//     lastName: 'Manal',
+//     email: 'mafruha.m@example.com',
+// })
 
 // console.log(result);
 
+const result = await db.query.users.findFirst()
+console.log(result)
+// need to run migration for tables to exist w these things? 
+// i also cd'd into backend then ran npx tsx ./src/db/db.ts  
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,5 +1,36 @@
-import { sqliteTable, integer } from "drizzle-orm/sqlite-core"
+import { sqliteTable } from "drizzle-orm/sqlite-core"
+import * as t from "drizzle-orm/sqlite-core";
+import { relations } from 'drizzle-orm';
 
-export const users = sqliteTable('users', {
-    id: integer(),
-  });
+export const users = sqliteTable(
+  "users",
+  {
+    id: t.int().primaryKey({ autoIncrement: true }),
+    firstName: t.text("first_name"),
+    lastName: t.text("last_name"),
+    email: t.text().notNull(),
+  }
+);
+
+export const usersRelations = relations(users, ({ many }) => ({
+	videos: many(videos),
+}));
+
+export const videos = sqliteTable(
+  "videos",
+  {
+    id: t.int().primaryKey({ autoIncrement: true}),
+    authorId: t.int("author_id").notNull(),
+    title: t.text().notNull(),
+    description: t.text(),
+    hlsPath: t.text("hls_path"),
+    thumbnailPath: t.text("thumbnail_path"),
+    status: t.text().$type<"processing" | "ready" | "failed">(),
+    duration: t.int(),
+    createdAt: t.int({ mode: 'timestamp_ms'}).notNull(),
+  }
+)
+
+export const videoRelations = relations(videos, ({ one }) => ({
+	author: one(users, { fields: [videos.authorId], references: [users.id] }),
+}));
