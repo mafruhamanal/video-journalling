a29b31fa352881d229069dd9cc32639c61b677b9
mafruhamanal <mafruhamanal1@gmail.com>
2025-12-24 16:47:30 -0500

    user upload endpoint works w multer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,20 +23,38 @@ function App() {
   },  [])
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>My name is {name}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-         Count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div>
+        {/* <form action="/stats" enctype="multipart/form-data" method="post">
+          <div>
+            <input type="file" name="video">
+            <input type="text" placeholder="your name" name="userId">
+            <input type="submit" value="Post my video!">
+          </div>
+        </form> */}
+
+        <form action="http://localhost:3000/videos/upload" encType='multipart/form-data' method='post'>
+          
+          <div>
+            <input type="file" name="video"/>
+            <input type="text" placeholder="enter your title" name="title"/>
+            <input type="text" placeholder="enter your name" name="userId"/>
+            <input type="submit" value="Post my video!"/>
+          </div>
+
+        </form>
+        <h1 className='text-3xl font-bold underline'>My name is {name}</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
